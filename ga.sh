3e711f21fe686a9b3dfd5fbfb3ddacc15cd66e35
Adam Jensen <acj@cps-26-236.cse.msu.edu>
2009-03-25 17:06:18 -0400

    - updated start script

--- ga.sh
+++ ga.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-javac -cp .:ec/refactor/jgrapht-jdk1.6.jar ec/refactor/*.java &&
-java -cp .:ec/refactor/jgrapht-jdk1.6.jar ec.Evolve -file refactor.params
+../java6/bin/javac -cp .:ec/refactor/jgrapht-jdk1.6.jar ec/refactor/*.java &&
+../java6/bin/java -cp .:ec/refactor/jgrapht-jdk1.6.jar ec.Evolve -file refactor.params
 #java -cp .:ec/refactor/jgrapht-jdk1.6.jar -Xmx512m ec.display.Console -file refactor.params
