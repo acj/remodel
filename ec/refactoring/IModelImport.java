e71f62dd5b152f250d0a9d61237ee6c7284e8484
Adam Jensen <acj@betelgeuse.cse.msu.edu>
2009-04-07 17:36:16 -0400

    - finishing up after rebase - deleted old JavexImport file - updated IModelImport interface to throw proper exception

--- ec/refactoring/IModelImport.java
+++ ec/refactoring/IModelImport.java
@@ -1,6 +1,8 @@
 package ec.refactoring;
 
+import java.io.FileNotFoundException;
+
 public interface IModelImport {
 	public void Import(AnnotatedGraph<AnnotatedVertex,AnnotatedEdge> g,
-			String Filename);
+			String Filename) throws FileNotFoundException;
 }
--- ec/refactoring/JavexImport.java
+++
@@ -1,9 +0,0 @@
-package ec.refactoring;
-
-public class JavexImport implements IModelImport {
-	public void Import(AnnotatedGraph<AnnotatedVertex, AnnotatedEdge> g,
-						String Filename) {
-		
-	}
-
-}
