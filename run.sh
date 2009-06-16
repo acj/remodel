e1e4acfe07c6a823b59188e829c207be6a4229c2
Adam Jensen <acj@cps-26-201.cse.msu.edu>
2009-06-16 16:29:54 -0400

    - New run.sh has options for storing results in a convenient way

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#
+# Usage: ./run.sh [date] [run name]
+#
+# e.g., ./run.sh 20090616 ReMoDD-Model
 JAVAC=/usr/bin/javac
 JAVA=/usr/bin/java
 #JAVAC=~/research/java6/bin/javac
@@ -8,11 +12,20 @@ JAVA=/usr/bin/java
 mkdir -p output
 rm -f output/*
 
+JAVACP=.:ec/refactoring/jgrapht-jdk1.6.jar:nsuml1_4/lib/nsmdf.jar:nsuml1_4/build/nsuml1_3.jar:hsqldb/lib/hsqldb.jar:xerces-2_9_1/xercesImpl.jar:xerces-2_9_1/xml-apis.jar
+
 #$JAVAC -g -cp .:ec/refactoring/jgrapht-jdk1.6.jar ec/refactoring/*.java && $JAVA -cp .:ec/refactoring/jgrapht-jdk1.6.jar:tijmp.jar -agentlib:tijmp -Xmx768m -enableassertions ec.Evolve -file ec/refactoring/refactoring.params
-$JAVAC -g -cp .:ec/refactoring/jgrapht-jdk1.6.jar ec/refactoring/*.java && $JAVA -cp .:ec/refactoring/jgrapht-jdk1.6.jar:hsqldb/lib/hsqldb.jar:tijmp.jar -Xmx1024m -enableassertions ec.Evolve -file ec/refactoring/refactoring.params
+$JAVAC -g -cp $JAVACP ec/refactoring/*.java && $JAVA -cp $JAVACP -Xmx3000m -enableassertions ec.Evolve -file ec/refactoring/refactoring.params
+
+if [ -n "$1" ]; then
+    if [ -n "$2" ]; then
+	mkdir -p results/$1/$2
+	cp out.stat output/* results/$1/$2
+    fi
+fi
 
 echo "Processing .dot files..."
-for i in `ls output/*.dot`
+for i in `ls output/pattern*.dot`
 do
     dot -Tpng $i -o $i.png
 done
