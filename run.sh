76f0a87f3d8095ac3d487a089b5b8b4fc9cba009
Adam Jensen <acj@maclaurin.local>
2009-07-03 12:44:40 -0400

    - Moved .jar files to lib/

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ JAVA=/usr/bin/java
 mkdir -p output
 rm -f output/*
 
-JAVACP=.:ec/refactoring/jgrapht-jdk1.6.jar:nsuml1_4/lib/nsmdf.jar:nsuml1_4/build/nsuml1_3.jar:hsqldb/lib/hsqldb.jar:xerces-2_9_1/xercesImpl.jar:xerces-2_9_1/xml-apis.jar
+JAVACP=.:lib/jgrapht-jdk1.6.jar:lib/hsqldb.jar:lib/xercesImpl.jar:lib/xml-apis.jar
 
 #$JAVAC -g -cp .:ec/refactoring/jgrapht-jdk1.6.jar ec/refactoring/*.java && $JAVA -cp .:ec/refactoring/jgrapht-jdk1.6.jar:tijmp.jar -agentlib:tijmp -Xmx768m -enableassertions ec.Evolve -file ec/refactoring/refactoring.params
 $JAVAC -g -cp $JAVACP ec/refactoring/*.java && $JAVA -cp $JAVACP -Xmx3000m -enableassertions ec.Evolve -file ec/refactoring/refactoring.params
