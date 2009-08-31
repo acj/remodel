068d559c724da6aec7d9278f097eccf8c7666c07
Adam Jensen <ajensen@linuxguy.org>
2009-08-31 17:25:03 -0400

    - Fixed run script (old change that was never committed)

--- run.sh
+++ run.sh
@@ -5,17 +5,13 @@
 # e.g., ./run.sh 20090616 ReMoDD-Model
 JAVAC=/usr/bin/javac
 JAVA=/usr/bin/java
-#JAVAC=~/research/java6/bin/javac
-#JAVA=~/research/java6/bin/java
 
 # Clean up
 mkdir -p output
 rm -f output/*
 
 JAVACP=.:lib/jgrapht-jdk1.6.jar:lib/hsqldb.jar:lib/xercesImpl.jar:lib/xml-apis.jar
-
-#$JAVAC -g -cp .:ec/refactoring/jgrapht-jdk1.6.jar ec/refactoring/*.java && $JAVA -cp .:ec/refactoring/jgrapht-jdk1.6.jar:tijmp.jar -agentlib:tijmp -Xmx768m -enableassertions ec.Evolve -file ec/refactoring/refactoring.params
-$JAVAC -g -cp $JAVACP ec/refactoring/*.java && $JAVA -cp $JAVACP -Xmx3000m -enableassertions ec.Evolve -file ec/refactoring/refactoring.params
+$JAVAC -g -cp $JAVACP ec/refactoring/*.java && $JAVA -cp $JAVACP -Xmx1000m -enableassertions ec.Evolve -file ec/refactoring/refactoring.params
 
 if [ -n "$1" ]; then
     if [ -n "$2" ]; then
