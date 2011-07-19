aa089fb99af572017fa43122cecdc35b0c92431c
Adam Jensen <acj@linuxguy.org>
2011-07-19 12:13:33 -0400

    - Removed obselete run script - Add ReMoDD XMI to Models/

--- ga.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-../java6/bin/javac -cp .:ec/refactor/jgrapht-jdk1.6.jar ec/refactor/*.java &&
-../java6/bin/java -cp .:ec/refactor/jgrapht-jdk1.6.jar ec.Evolve -file refactor.params
-#java -cp .:ec/refactor/jgrapht-jdk1.6.jar -Xmx512m ec.display.Console -file refactor.params
